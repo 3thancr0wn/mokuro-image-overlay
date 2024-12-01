1cedbe913a866501ec904580035703125c4789b7
minierparedes <3thancr0wn@gmail.com>
2024-12-01 22:48:06 +0900

    Initial Commit

---
+++ MokuroImageOverlay2/MokuroImageOverlay2App.swift
@@ -0,0 +1,17 @@
+//
+//  MokuroImageOverlay2App.swift
+//  MokuroImageOverlay2
+//
+//  Created by ethan crown on 2024/12/01.
+//
+
+import SwiftUI
+
+@main
+struct MokuroImageOverlay2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
