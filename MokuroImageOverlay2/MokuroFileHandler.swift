f6497fa14843460d9c38c88c318cd5c60d7a53ba
minierparedes <3thancr0wn@gmail.com>
2024-12-02 12:15:04 +0900

    feat(fileMangager)add mokuro file handler

--- MokuroImageOverlay2/MokuroFileHandler.swift
+++ MokuroImageOverlay2/MokuroFileHandler.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct MokuroFileHandler {
+    static func readMokuroFile(filePath: String) -> MokuroData? {
+        do {
+            let fileURL = URL(fileURLWithPath: filePath)
+            let data = try Data(contentsOf: fileURL)
+            let decoder = JSONDecoder()
+            
+            // Adjust decoding strategy if needed
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(MokuroData.self, from: data)
+        } catch {
+            print("Error reading .mokuro file: \(error)")
+            return nil
+        }
+    }
+}
