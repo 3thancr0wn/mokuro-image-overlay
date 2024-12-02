817b54629eabafac07c1867a6d9fd797691cae4c
minierparedes <3thancr0wn@gmail.com>
2024-12-02 12:14:10 +0900

    refactor(model)conform to codable

--- MokuroImageOverlay2/MokuroData.swift
+++ MokuroImageOverlay2/MokuroData.swift
@@ -15,7 +15,7 @@ extension CGPoint: Hashable {
     }
 }
 // Define a structure for each "block" that contains the text and its properties
-struct MokuroBlock: Identifiable,Hashable {
+struct MokuroBlock: Identifiable, Hashable, Codable {
     var id = UUID() // Unique ID for each block
     var box: [CGFloat] // [x1, y1, x2, y2] for bounding box of the block
     var vertical: Bool // Whether the text is vertical
@@ -25,7 +25,7 @@ struct MokuroBlock: Identifiable,Hashable {
 }
 
 // Define the main MokuroData model that will hold the entire data
-struct MokuroData {
+struct MokuroData: Codable {
     var version: String
     var imgWidth: CGFloat
     var imgHeight: CGFloat
