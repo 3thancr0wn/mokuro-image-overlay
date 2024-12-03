bfc5624f94027fcb5a678e8f96c7ae714c0fc2c1
minierparedes <3thancr0wn@gmail.com>
2024-12-03 13:02:30 +0900

    feat(utils)add func loading files & scaleBox

--- MokuroImageOverlay2/MokuroUtils.swift
+++ MokuroImageOverlay2/MokuroUtils.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import CoreGraphics
+
+struct MokuroUtils {
+    // Load mokuro data
+    static func loadMokuroData(fileName: String) -> MokuroData? {
+        if let fileURL = Bundle.main.url(forResource: fileName, withExtension: "mokuro") {
+            return MokuroFileHandler.loadMokuroData(from: fileURL)
+        }
+        return nil
+    }
+    
+    // Scale the block box coordinates
+    static func scaleBox(_ box: [CGFloat], scaleX: CGFloat, scaleY: CGFloat) -> CGRect {
+        let x = box[0] * scaleX
+        let y = box[1] * scaleY
+        let width = (box[2] - box[0]) * scaleX
+        let height = (box[3] - box[1]) * scaleY
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+}
+
