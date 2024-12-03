e9fbe9255799518e7ee03a1cc539e40bd954be06
minierparedes <3thancr0wn@gmail.com>
2024-12-03 13:00:20 +0900

    feat(class)add views & utils classes

---
+++ MokuroImageOverlay2/MokuroUtils.swift
@@ -0,0 +1,8 @@
+//
+//  MokuroUtils.swift
+//  MokuroImageOverlay2
+//
+//  Created by ethan crown on 2024/12/03.
+//
+
+import Foundation
---
+++ MokuroImageOverlay2/TategakiTextView.swift
@@ -0,0 +1,8 @@
+//
+//  TategakiTextView.swift
+//  MokuroImageOverlay2
+//
+//  Created by ethan crown on 2024/12/03.
+//
+
+import Foundation
